3ba502db4626053c91a801a88221b0579a3320c8
Olena Salnikova <nameless.hire5@gmail.com>
2025-11-20 18:38:11 -0500

    HW 23.1. Intro to Playwright

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    rules: {
+      "semi": ["error", "always"], // Require semicolons
+      "prefer-const": "error" // Prefer const when variables are not reassigned
+    },
+    languageOptions: { globals: globals.browser } 
+  },
+]);
